89e5e275e9fa4fd483d9a6bf9404edbdba1899f0
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2016-01-24 11:01:36 -0800

    Ad

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ config = {
 
   // Production (Heroku)
   production: {
-    url: process.env.HEROKU_URL,
+    url: 'blog.legionanalytics.com'
     mail: {
       transport: 'SMTP',
       options: {
